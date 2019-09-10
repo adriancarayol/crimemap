6191163d117622adb17d1e267ba80df0089ce775
Adrian <adriancarayol@gmail.com>
2019-09-10 23:03:38 +0200

    Including markers in map

--- crimemap/lib/crimemap_web/views/crime_view.ex
+++ crimemap/lib/crimemap_web/views/crime_view.ex
@@ -1,3 +1,14 @@
 defmodule CrimemapWeb.CrimeView do
   use CrimemapWeb, :view
+
+  def parse_latitude(point) do
+    coordinates = Tuple.to_list(point.coordinates)
+    "#{List.first(coordinates)}"
+  end
+
+  def parse_longitude(point) do
+    coordinates = Tuple.to_list(point.coordinates)
+    "#{List.last(coordinates)}"
+  end
+
 end
